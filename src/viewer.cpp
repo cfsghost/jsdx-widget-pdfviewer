612241fd452435b96927076654a7242abcaf33cc
Fred Chien <fred@mandice.com>
2012-04-14 08:11:51 +0800

    completed a API to load PDF

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -13,6 +13,8 @@
 #include "jsdx_widget_pdfviewer.hpp"
 #include "viewer.hpp"
 
+#define IMAGE_DPI 150
+
 namespace JSDXWidgetPDFViewer {
  
 	using namespace node;
@@ -23,7 +25,7 @@ namespace JSDXWidgetPDFViewer {
 		HandleScope scope;
 
 		_actor = NULL;
-		LoadFinishedFunc = new Persistent<Function>();
+		document = NULL;
 	}
 
 	void Viewer::Initialize(Handle<Object> target)
@@ -45,12 +47,9 @@ namespace JSDXWidgetPDFViewer {
 		HandleScope scope;
 
 		Actor::PrototypeMethodsInit(constructor_template);
-		constructor_template->InstanceTemplate()->SetAccessor(String::NewSymbol("filterQuality"), Viewer::FilterQualityGetter, Viewer::FilterQualitySetter);
+//		constructor_template->InstanceTemplate()->SetAccessor(String::NewSymbol("filterQuality"), Viewer::FilterQualityGetter, Viewer::FilterQualitySetter);
 
 		NODE_SET_PROTOTYPE_METHOD(constructor_template, "loadFile", Viewer::LoadFile);
-		NODE_SET_PROTOTYPE_METHOD(constructor_template, "loadFileSync", Viewer::LoadFileSync);
-		NODE_SET_PROTOTYPE_METHOD(constructor_template, "keepAspectRatio", Viewer::KeepAspectRatio);
-		NODE_SET_PROTOTYPE_METHOD(constructor_template, "on", Viewer::On);
 	}
 
 	/* ECMAScript constructor */
@@ -70,21 +69,10 @@ namespace JSDXWidgetPDFViewer {
 
 		/* Create Viewer */
 		obj->_actor = clutter_texture_new();
+		obj->cr = clutter_cairo_texture_create(CLUTTER_CAIRO_TEXTURE(obj->_actor));
+		cairo_scale(obj->cr, IMAGE_DPI/72.0, IMAGE_DPI/72.0);
 
 		return scope.Close(args.This());
-	}
-
-	void Viewer::_LoadFile(Viewer *texture, const char *filename, bool hasCallback)
-	{
-		CoglHandle cogltex;
-		ClutterActor *instance = texture->_actor;
-
-//		if (hasCallback)
-//			g_signal_connect(G_OBJECT(instance), "load-finished", G_CALLBACK(Viewer::_LoadFinishedCallback), (gpointer)texture);
-
-		clutter_texture_set_load_async(CLUTTER_TEXTURE(instance), TRUE);
-
-		clutter_texture_set_from_file(CLUTTER_TEXTURE(instance), filename, NULL);
 	}
 
 	Handle<Value> Viewer::LoadFile(const Arguments &args)
@@ -92,76 +80,9 @@ namespace JSDXWidgetPDFViewer {
 		HandleScope scope;
 
 		if (args[0]->IsString()) {
-			Viewer *texture = ObjectWrap::Unwrap<Viewer>(args.This());
+			Viewer *viewer = ObjectWrap::Unwrap<Viewer>(args.This());
 
-			if (args[1]->IsFunction()) {
-				*(texture->LoadFinishedFunc) = Persistent<Function>::New(Handle<Function>::Cast(args[1]));
-				_LoadFile(texture, *String::Utf8Value(args[0]->ToString()), True);
-			} else {
-				_LoadFile(texture, *String::Utf8Value(args[0]->ToString()), False);
-			}
-		}
-
-		return args.This();
-	}
-
-	Handle<Value> Viewer::LoadFileSync(const Arguments &args)
-	{
-		HandleScope scope;
-
-		if (args[0]->IsString()) {
-			Actor *actor = ObjectWrap::Unwrap<Actor>(args.This());
-			ClutterActor *instance = ObjectWrap::Unwrap<Actor>(args.This())->_actor;
-			clutter_texture_set_load_async(CLUTTER_TEXTURE(instance), FALSE);
-			
-			return scope.Close(
-				Boolean::New(clutter_texture_set_from_file(CLUTTER_TEXTURE(instance), *String::Utf8Value(args[0]->ToString()), NULL))
-			);
-
-			/* TODO: Need callback function for file loaded */
-		}
-
-		return scope.Close(
-			Boolean::New(False)
-		);
-	}
-
-	Handle<Value> Viewer::FilterQualityGetter(Local<String> name, const AccessorInfo& info)
-	{
-		HandleScope scope;
-
-		ClutterActor *instance = ObjectWrap::Unwrap<Actor>(info.This())->_actor;
-
-		return scope.Close(
-			Integer::New(clutter_texture_get_filter_quality(CLUTTER_TEXTURE(instance)))
-		);
-	}
-
-	void Viewer::FilterQualitySetter(Local<String> name, Local<Value> value, const AccessorInfo& info)
-	{
-		HandleScope scope;
-
-		if (value->IsNumber()) {
-			ClutterActor *instance = ObjectWrap::Unwrap<Actor>(info.This())->_actor;
-
-//			clutter_texture_set_filter_quality(CLUTTER_TEXTURE(instance), (ClutterViewerQuality)value->ToInteger()->Value());
-		}
-	}
-
-	Handle<Value> Viewer::KeepAspectRatio(const Arguments &args)
-	{
-		HandleScope scope;
-
-		ClutterActor *instance = ObjectWrap::Unwrap<Actor>(args.This())->_actor;
-
-		/* Return value if no parameter */
-		if (args[0]->IsUndefined() || args[0]->IsNull())
-			return scope.Close(
-				Boolean::New(clutter_texture_get_keep_aspect_ratio(CLUTTER_TEXTURE(instance)))
-			);
-
-		if (args[0]->IsBoolean()) {
-			clutter_texture_set_keep_aspect_ratio(CLUTTER_TEXTURE(instance), args[0]->ToBoolean()->Value());
+			viewer->document = poppler_document_new_from_file(*String::Utf8Value(args[0]->ToString()), NULL, NULL);
 		}
 
 		return args.This();
--- src/viewer.hpp
+++ src/viewer.hpp
@@ -16,24 +16,18 @@ namespace JSDXWidgetPDFViewer {
 		static void Initialize(v8::Handle<v8::Object> target);
 		static void PrototypeMethodsInit(v8::Handle<v8::FunctionTemplate> constructor_template);
 
-		v8::Persistent<v8::Function> *LoadFinishedFunc;
-
 		PopplerDocument *document;
+		cairo_t *cr;
 
 	protected:
 		Viewer();
 
 		static v8::Handle<v8::Value> New(const v8::Arguments& args);
 
-		static void _LoadFile(Viewer *texture, const char *filename, bool hasCallback);
 		static v8::Handle<v8::Value> LoadFile(const v8::Arguments& args);
-		static v8::Handle<v8::Value> LoadFileSync(const v8::Arguments& args);
 
-		static v8::Handle<v8::Value> FilterQualityGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info);
-		static void FilterQualitySetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::AccessorInfo& info);
-
-		static v8::Handle<v8::Value> KeepAspectRatio(const v8::Arguments& args);
-		static v8::Handle<v8::Value> On(const v8::Arguments& args);
+//		static v8::Handle<v8::Value> FilterQualityGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info);
+//		static void FilterQualitySetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::AccessorInfo& info);
 	};
 
 }
